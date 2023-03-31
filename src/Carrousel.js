29166dd4cc55a1dbe8c635995f5a202ca83c6ea8
romainlauret <71262737+romainlauret@users.noreply.github.com>
2023-03-31 14:45:10 +0200

    Update Carrousel.js

--- src/Carrousel.js
+++ src/Carrousel.js
@@ -9,17 +9,17 @@ export default function Carrousel() {
       <img
         className={"Obj-contain" + (toDisp === 0 ? "" : " Hiden")}
         alt="lol"
-        src="https://github.com/yannkozon/hackathon-eco-conception/blob/main/assets/images/slide-1.jpg?raw=true"
+        src="https://github.com/GordonWolf/ecoconception/blob/main/src/assets/images/slide-1.webp?raw=true"
       />
       <img
         className={"Obj-contain" + (toDisp === 1 ? "" : " Hiden")}
         alt="lol"
-        src="https://github.com/yannkozon/hackathon-eco-conception/blob/main/assets/images/slide-2.jpg?raw=true"
+        src="https://github.com/GordonWolf/ecoconception/blob/main/src/assets/images/slide-2.webp?raw=true"
       />
       <img
         className={"Obj-contain" + (toDisp === 2 ? "" : " Hiden")}
         alt="lol"
-        src="https://github.com/yannkozon/hackathon-eco-conception/blob/main/assets/images/slide-3.jpg?raw=true"
+        src="https://github.com/GordonWolf/ecoconception/blob/main/src/assets/images/slide-3.webp?raw=true"
       />
       <div className="Nav-Carrousel">
         <div onClick={() => setDisp((toDisp - 1) % 3)}>"ok"</div>
