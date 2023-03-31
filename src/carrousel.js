b64bd1aaf8032d7b24219f587911e5a5c265e370
romainlauret <71262737+romainlauret@users.noreply.github.com>
2023-03-31 13:29:19 +0200

    Create carrousel.js

---
+++ src/carrousel.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [toDisp, setDisp] = useState(0);
+
+  return (
+    <div className="Carrousel">
+      <img
+        className={"Obj-contain" + (toDisp === 0 ? "" : " Hiden")}
+        alt="lol"
+        src="https://github.com/yannkozon/hackathon-eco-conception/blob/main/assets/images/slide-1.jpg?raw=true"
+      />
+      <img
+        className={"Obj-contain" + (toDisp === 1 ? "" : " Hiden")}
+        alt="lol"
+        src="https://github.com/yannkozon/hackathon-eco-conception/blob/main/assets/images/slide-2.jpg?raw=true"
+      />
+      <img
+        className={"Obj-contain" + (toDisp === 2 ? "" : " Hiden")}
+        alt="lol"
+        src="https://github.com/yannkozon/hackathon-eco-conception/blob/main/assets/images/slide-3.jpg?raw=true"
+      />
+      <div className="Nav-Carrousel">
+        <div onClick={() => setDisp((toDisp - 1) % 3)}>"ok"</div>
+        <div onClick={() => setDisp((toDisp + 1) % 3)}>"ok"</div>
+      </div>
+    </div>
+  );
+}
